d667cc01789730140a5f047e2775af604e958f98
qed0711 <qdizon@gmail.com>
2018-12-13 14:47:53 -0500

    file structure and App component structure

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+
+    render(){
+        return(
+            <div>
+                This is the main content.
+            </div>
+        )
+    }
+
+}
+
+export default Main;
---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Navigation extends Component {
+
+    render(){
+        return(
+            <div>
+                "hello world"
+            </div>
+        )
+    }
+
+}
+
+export default Navigation;
