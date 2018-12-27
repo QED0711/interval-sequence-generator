b0098fbaf955a88f5f637f5db70f13b884f22d07
qed0711 <qdizon@gmail.com>
2018-12-27 05:49:50 -0500

    added Chord Class to newGenerateFromSet

--- src/js/newGenerator.js
+++ src/js/newGenerator.js
@@ -1,4 +1,5 @@
 
+
 function pcsFromSequence(set, sequence){
     let chord = {0: true, size: 1}
     let prevPC = 0
@@ -16,8 +17,47 @@ function pcsFromSequence(set, sequence){
     return chord;
 }
 
+class Chord {
+    constructor(set, sequence){
+        this.sequence = sequence;
+        this.designation = `(${set})-0-<${sequence}>`;
+        this.pcs = Chord.pcsFromSequence(set, sequence)        
+    }
 
-// console.log(pcsFromSequence([3,4], [0, 1, 1, 0]))
+    static chordFromDesignation(designation){
+        let info = designation.split("-");
+        let set = info[0].match(/\d+/g).map(x => parseInt(x));
+        let startingPitch = parseInt(info[1]);
+        let sequence = info[2].match(/\d+/g).map(x => parseInt(x));
+        let chord = [startingPitch];
+        for(let i = 0; i < sequence.length; i++){
+            let nextPC = chord[chord.length - 1] + set[sequence[i]];
+            if(nextPC >= 12) {nextPC = nextPC - 12}
+            chord.push(nextPC) 
+        }
+        console.log(chord)
+    }
+
+    static pcsFromSequence(set, sequence){
+        let chord = {0: true, size: 1}
+        let prevPC = 0
+        for(let i = 0; i < sequence.length; i++){
+            let nextPC = prevPC + set[sequence[i]];
+            if(nextPC >= 12) nextPC = nextPC - 12;
+            if(!chord[nextPC]){
+                chord[nextPC] = true;
+                chord.size += 1;
+            } else {
+                return false;
+            }
+            prevPC = nextPC
+        }
+        return chord;
+    }
+
+}
+
+Chord.chordFromDesignation("(10,11)-0-<1,0,0,1,0,0>");
 
 function chordInfo(set, sequence){
     return {
@@ -27,40 +67,47 @@ function chordInfo(set, sequence){
     }
 }
 
-
-// console.log(chordInfo([3,4], [0,1]))
-
 function updateChordInfo(set, chord){
     chord.designation = `(${set})-0-<${chord.sequence}>`,
     chord.pcs = pcsFromSequence(set, chord.sequence);
 }
 
 function newGenerateFromSet(set){
+    set = set.sort((a,b) => a - b)
     let validChords = [];
 
-    function createSequence(set, currentChord){
-        // console.log(currentChord)
-        if(!currentChord.pcs) return
-        if(currentChord.pcs.size > 12) return
+    function createSequence(set, currentChord){  
+        // Base cases occur before recersive call
+        // we can guarantee that any chord that enters here is valid.   
+        // This is where the filtering should occur
 
-        if(currentChord.sequence.length > 0){
+        if(currentChord.sequence.length > 0){ // this conditaional allows for the first empty chord to pass without being pushed to the valid chords
             validChords.push(currentChord);
         }
+        // Iterate through the interval options and generate new sequences
         for(let i = 0; i < set.length; i++){
-            let newChord = {designation: currentChord.designation};
-            newChord.sequence = [...currentChord.sequence];
-            newChord.pcs = {...currentChord.sequence};
-
+            // copy the currentChord to a new chord for operations
+            let newChord = {
+                sequence: [...currentChord.sequence],
+                designation: currentChord.designation,
+                pcs: {...currentChord.pcs}
+            };
+            // Add new interval to end of chord
             newChord.sequence.push(i);
             updateChordInfo(set, newChord);
+            // Base cases: Put here so createSequence only called on valid chords
+            if(!newChord.pcs) continue;
+            if(newChord.pcs.size > 12) continue;
+            // Recursive call to createSequence
             createSequence(set, newChord);
         }
     }
-
+    // Call initial createSequence with an empty sequence 
     createSequence(set, chordInfo(set, []));
+    // return validChords after createSequence has completed
     return validChords;
 }
 
-console.time("Timer")
-console.log(newGenerateFromSet([3,7,10,8]).length)
-console.timeEnd("Timer")
+// console.time("Timer")
+// console.log(newGenerateFromSet([3,4,5,6,7,8]).length)
+// console.timeEnd("Timer")
